eeccbc2440a63323e5f923888e45a0ae6c4a5932
Jan Stępień <jstepien@users.sourceforge.net>
2014-02-24 22:11:05 +0100

    Add a tool for preciser timing measurements

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -5,19 +5,29 @@ require 'tempfile'
 model = ARGV[0]
 puts "original,encoded,ratio,t_enc,t_dec"
 tmpfile = Dir::Tmpname.make_tmpname "tmp", nil
+n_enc = 32
+n_dec = 64
 begin
   while ex = STDIN.gets do
+    IO.popen "sh -c '../skr #{model} > #{tmpfile}'", "w" do |io|
+      io.write ex
+    end
     enc = Benchmark.measure do
-      IO.popen "sh -c '../skr #{model} > #{tmpfile}'", "w" do |io|
+      IO.popen "./measure -c #{n_enc} #{model}", "w" do |io|
         io.write ex
       end
     end
-    len = `wc -c #{tmpfile}`.to_i
+    comp = File.read(tmpfile)
+    len = comp.length
+    decomp = `cat #{tmpfile} | ../unskr #{model}`
+    raise ex unless decomp == ex
     dec = Benchmark.measure do
-      decomp = `cat #{tmpfile} | ../unskr #{model}`
-      raise ex unless decomp == ex
+      IO.popen "./measure -d #{n_dec} #{model}", "w" do |io|
+        io.write comp
+      end
     end
-    result = [ex.length, len, len * 1.0 / ex.length, enc.real, dec.real]
+    ratio = len * 1.0 / ex.length
+    result = [ex.length, len, ratio, enc.real / n_enc, dec.real / n_dec]
     puts result.join(",")
   end
 ensure
---
+++ benchmark/measure.rs
@@ -0,0 +1,50 @@
+use std::io::{stdin, File};
+use std::libc::{size_t, free, c_void};
+use std::os::args;
+use std::path::Path;
+use std::ptr::null;
+use std::vec::raw::from_buf_raw;
+
+extern {
+  fn skr_compress(model: *u8, modlen: size_t,
+                  inp: *u8, inlen: size_t,
+                  outp: **u8, outlen: size_t) -> size_t;
+  fn skr_decompress(model: *u8, modlen: size_t,
+                    inp: *u8, inlen: size_t,
+                    outp: **u8, outlen: size_t) -> size_t;
+}
+
+type F = extern "C" unsafe fn(*u8, size_t, *u8, size_t, **u8, size_t) -> size_t;
+
+fn process_vec(inp: &[u8], inp2: &[u8], fun: F) -> ~[u8] {
+  unsafe {
+    let buf = null();
+    let nout = fun(inp.as_ptr(), inp.len() as size_t,
+                   inp2.as_ptr(), inp2.len() as size_t,
+                   &buf, 0);
+    assert!(buf != null() && nout > 0);
+    let out = from_buf_raw(buf, nout as uint);
+    free(buf as *c_void);
+    out
+  }
+}
+
+fn measure(model_file: ~str, attempts: uint, fun: F) {
+  let input = stdin().read_to_end();
+  let model = File::open(&Path::new(model_file)).read_to_end();
+  for _ in range(0, attempts) {
+    process_vec(model, input, fun);
+  }
+}
+
+fn main() {
+  let args = args();
+  assert!(args.len() == 4);
+  let attempts = from_str(args[2]).unwrap();
+  let f = match args[1] {
+    ~"-d" => skr_decompress,
+    ~"-c" => skr_compress,
+    _     => fail!(),
+  };
+  measure(args[3], attempts, f)
+}
