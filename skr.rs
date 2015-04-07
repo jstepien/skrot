e79b8a041e6c19f9afd5898367191ba6bcffe675
Jan Stępień <jstepien@users.sourceforge.net>
2015-04-07 11:52:05 +0200

    Compatibility with the latest Rust nightly

--- skr.rs
+++ skr.rs
@@ -1,10 +1,12 @@
+#![feature(libc)]
 extern crate libc;
 
-use std::io::{stdin, stdout, File};
+use std::io::{stdin, stdout, Read, Write};
+use std::fs::{File};
 use libc::{size_t, free, c_void};
-use std::os::args;
+use std::env::args;
 use std::path::Path;
-use std::ptr::null_mut;
+use std::ptr::{null_mut, read};
 
 #[link(name = "skr")]
 extern {
@@ -24,13 +26,23 @@ extern {
 type Fun = unsafe extern "C" fn(*const u8, size_t, &*mut u8, size_t, *const u32) -> size_t;
 type Fun2 = unsafe extern "C" fn(*const u8, size_t, *const u8, size_t, &*mut u8, size_t, *const u32) -> size_t;
 
+fn from_raw_buf(buf: *const u8, len: usize) -> Vec<u8> {
+  let mut vec = Vec::with_capacity(len);
+  unsafe {
+    for i in 0..len as isize {
+      vec.push(read(buf.offset(i)));
+    }
+  }
+  vec
+}
+
 fn process_vec(inp: Vec<u8>, fun: Fun) -> Vec<u8> {
   unsafe {
     let default_opts = 0;
     let buf = null_mut();
     let nout = fun(inp.as_ptr(), inp.len() as size_t, &buf, 0, &default_opts);
     assert!(!buf.is_null() && nout > 0);
-    let out = Vec::from_raw_buf(buf as *const u8, nout as usize);
+    let out = from_raw_buf(buf as *const u8, nout as usize);
     free(buf as *mut c_void);
     out
   }
@@ -45,22 +57,27 @@ fn process_vec2(inp: Vec<u8>, inp2: Vec<u8>, fun: Fun2) -> Vec<u8> {
                    &buf, 0,
                    &default_opts);
     assert!(!buf.is_null() && nout > 0);
-    let out = Vec::from_raw_buf(buf as *const u8, nout as usize);
+    let out = from_raw_buf(buf as *const u8, nout as usize);
     free(buf as *mut c_void);
     out
   }
 }
 
 fn main() {
-  let args = args();
-  let model = |&:| {
-    assert!(args.len() == 2);
-    let model_file = args[1].as_slice();
-    File::open(&Path::new(model_file)).read_to_end().unwrap()
+  let model = || {
+    let model_file = args().nth(1).unwrap();
+    let mut buf = Vec::new();
+    let path = Path::new(&model_file);
+    File::open(path).unwrap().read_to_end(&mut buf).unwrap();
+    buf
   };
   let output = {
-    let input = |&:| { stdin().read_to_end().unwrap() };
-    let prog = args[0].as_slice();
+    let input = || {
+      let mut buf = Vec::new();
+      Read::read_to_end(&mut stdin(), &mut buf).unwrap();
+      buf
+    };
+    let prog = args().nth(0).unwrap();
     if prog.ends_with("mkskr") {
       process_vec(input(), skr_model)
     } else if prog.ends_with("unskr") {
@@ -69,5 +86,5 @@ fn main() {
       process_vec2(model(), input(), skr_compress)
     }
   };
-  let _ = stdout().write(output.as_slice()).unwrap();
+  stdout().write(&output).unwrap();
 }
