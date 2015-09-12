e96f18dc0ea021bf3702c35137a3d39c217a16bf
Jan Stępień <jstepien@users.sourceforge.net>
2015-09-12 10:54:56 +0200

    Bump the Java library to 0.2

--- java/project.clj
+++ java/project.clj
@@ -1,4 +1,4 @@
-(defproject cc.stepien/skrot "0.1"
+(defproject cc.stepien/skrot "0.2"
   :dependencies [[commons-io/commons-io "2.4"]
                  [net.jpountz.lz4/lz4 "1.2.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
