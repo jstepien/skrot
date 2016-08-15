f5b2f9cf752d57f4dcbf1a64f18cfaf59f9027e2
Jan Stępień <jstepien@users.sourceforge.net>
2016-08-15 12:08:31 +0200

    Bump JVM version's test dependencies

--- java/project.clj
+++ java/project.clj
@@ -1,7 +1,7 @@
 (defproject cc.stepien/skrot "0.2"
   :dependencies [[commons-io/commons-io "2.4"]
                  [net.jpountz.lz4/lz4 "1.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/test.check "0.6.2"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [org.clojure/test.check "0.9.0"]]}}
   :java-source-paths ["src"]
   :javac-options ["-source" "1.6" "-target" "1.6"])
